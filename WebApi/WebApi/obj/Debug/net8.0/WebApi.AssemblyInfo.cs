4829f8d58edb6b74f7dd4141f2a82347277aad80
kirtikonka <kirtikonka0506@gmail.com>
2024-10-01 17:35:36 +0530

    Userinterface

--- WebApi/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f80ae540699821d746d658727edc95fd127ebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109e5f253c769464cec1cb6e98464b2d75bbf2cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

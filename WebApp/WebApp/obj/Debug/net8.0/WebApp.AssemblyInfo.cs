0525b4de0bc18d38002fd41ba3d1a754adec0115
prasad <prasadmhasal@gmail.com>
2024-10-04 00:35:20 +0530

    loding and dashboad icon with contact with namelatter

--- WebApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6a5498267b2b22c9f76c35a054c49a901946d6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+904ddb4e0dbaa53c74a9a005429a3064ad1e4f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
